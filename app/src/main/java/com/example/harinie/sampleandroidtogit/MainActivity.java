769f6a0db3725329b2689797d07c45091c79ee28
HarinieR <harinieravi05@gmail.com>
2018-07-14 22:36:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/harinie/sampleandroidtogit/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.harinie.sampleandroidtogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textClicked();
+            }
+        });
+    }
+
+    private void textClicked(){
+        Toast.makeText(this,"Text clicked", Toast.LENGTH_SHORT).show();
+    }
+
+}
